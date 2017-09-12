478e7c80e6c6cfc6176723d0d00bb420470d58d2
Abdullah <abdull4h1911@hotmail.com>
2017-09-12 11:21:39 +0200

    commit from pycharm,

--- Pytest.py
+++ Pytest.py
@@ -2,7 +2,7 @@ import pandas as pd
 df= pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data', header=None, sep='\s+')
 df.columns = ['CRIM','ZN', 'INDUS','CHAS','NOX','RM','AGE','DIS','RAD','TAX','PTRATIO','B','LSTAT','MEDV']
 df.head()
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set(style='whitegrid', context='notebook')
